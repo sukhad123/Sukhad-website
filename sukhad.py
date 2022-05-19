a2998fa92c0565b9c3129dfb9a77a6d81c0b8fb6
Sukhad Adhikari <Sukhad Adhikari>
2022-05-19 13:36:09 +0545

    first commit

---
+++ sukhad.py
@@ -0,0 +1,6 @@
+#Roshani Dangol
+Last_name, First_name, UID = input("Enter your last name, first name and user ID:").split()
+int_number, decimal_number = input("Please enter one integer and one decimal number:").split()
+int_number = int(int_number)
+decimal_number = float(decimal_number)
+print(decimal_number)
